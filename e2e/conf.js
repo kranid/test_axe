542dcc43cd9bf9905c7a8bcb9e01a5eedc494937
Konstantin Galiakhmetov <galiahmetov@rambler.ru>
2019-02-20 13:06:58 +0300

    update base link

--- e2e/conf.js
+++ e2e/conf.js
@@ -1,16 +1,16 @@
 // An example configuration file.
 exports.config = {
 
-  seleniumServerJar: './../node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.11.0.jar',
+  seleniumServerJar: './../node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-3.141.59.jar',
   port: 4445,
   //version and type of a binary may vary depends on OS type, check the binary version after npm install
-  chromeDriver: './../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.38.exe',
+  chromeDriver: './../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.46.exe',
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
     'browserName': 'chrome',
   },
-  baseUrl: 'http://devdemo.competentum.com/PEsci_PhysioEx/exercise-',
+  baseUrl: 'http://devdemo.competentum.com/PEsci_PhysioEx/exercise-1/ex01act0',
 
 
   // Framework to use. Jasmine is recommended.
